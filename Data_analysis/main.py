9ec3a2e80be7eb47e43fb552332146a0afac38d9
Fobi-02 <federicobattisti4@gmail.com>
2026-08-30 19:13:09 +0200

    changed main file

--- Data_analysis/main.py
+++
@@ -1,9 +0,0 @@
-from functions.read_csv import read_csv
-from functions.plot_markers import plot_markers
-from functions.plot_markers_slide import plot_markers_slider
-
-csv_file = "../Data/F-30_001.csv"
-df = read_csv(csv_file)
-
-#plot_markers(df, 100)
-plot_markers_slider(df, step=100)
