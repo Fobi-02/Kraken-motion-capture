cbe544894f810a025881194e2a772cb95815cefe
Fobi-02 <federicobattisti4@gmail.com>
2026-09-06 19:39:56 +0200

    F-50 map added

--- TODOs.md
+++ TODOs.md
@@ -6,7 +6,11 @@
 - [ ] Function to transform coordinates from mocap RF to vehicle RF
 - [ ] Minimization to associate all static (chassis) kinematic points
 - [ ] Function to compensate for missing data of certain points
-- [ ] Maps to assign two mocap points to a single suspension link with the correct name (1/17)
+- [ ] Maps to assign two mocap points to a single suspension link with the correct name (2/17)
+        - Use plot_markers to associate the names at frame 0
+        - Check if they are all present from the name of the columns of the csv file
+        - Probably some markers appear in later frames so the json file also have to be updated
+        - Pay attention to the steering markers (+ clockwise rotation, - counterclockwise rotation)
 - [x] Function to transform df based on maps
 - [x] Function to plot the chassis with links
 - [ ] Function to read the steering angle
--- data_analysis/main.py
+++ data_analysis/main.py
@@ -3,17 +3,27 @@ from functions.read_csv import read_csv
 from functions.plot import *
 from functions.map_points import *
 
-# loading the first file data
-csv_file = "../data/F-30_001.csv"
-json_file = "../data/marker_maps/F-30map.json"
-df = read_csv(csv_file)
-# renaming each marker to the correct name
-df = rename_markers(df, json_file)
-# finding the kinematic points given the markers
-df = marker_to_kinematic_points(df)
+file_select = "F-60"
 
-# Plotting the markers in a wanted frame
-frame = 2400
-plot_markers(df, frame)
-#plot_links(df, frame)
-plot_markers_slider(df)
+if file_select == "F-30":
+    csv_file = "../data/F-30_001.csv"
+    json_file = "../data/marker_maps/F-30map.json"
+    df = read_csv(csv_file)
+    # renaming each marker to the correct name
+    df = rename_markers(df, json_file)
+    # finding the kinematic points given the markers
+    #df = marker_to_kinematic_points(df)
+
+    # Plotting the markers in a wanted frame
+    frame = 2400
+    plot_markers(df, frame)
+    #plot_links(df, frame)
+    #plot_markers_slider(df)
+
+elif file_select == "F-60":
+    csv_file = "../data/F-60_001.csv"
+    json_file = "../data/marker_maps/F-60map.json"
+    df = read_csv(csv_file)
+    df = rename_markers(df, json_file)
+    #df = marker_to_kinematic_points(df)
+    plot_markers(df, 0)
